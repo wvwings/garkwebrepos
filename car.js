b6372d9a7938d7068903ca46e71e592765ec5b4e
EXPD <shenntykus@gmail.com>
2023-08-20 18:06:59 +0300

    okey321

--- car.js
+++ car.js
@@ -1,1 +1,8 @@
-// test
+let cells = document.querySelectorAll(".probu")
+// let cells = document.querySelectorAll('.cell')
+
+cells.forEach((cell)=>{
+    cell.addEventListener('click', ()=>{
+
+    })
+})
